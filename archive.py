ec38bedad27cab8f153ec91058a0c6d7e9fcc8f4
salex139s <sasha.13.9@yandex.ru>
2023-03-15 15:55:32 +0300

    Refactor code

--- archive.py
+++ archive.py
@@ -1,32 +1,53 @@
+"""
+Archiver microservice
+"""
 import os
 import zipfile
 import uuid
 import sys
 
 
-def zipdir(paths, ziph):
-    # ziph is zipfile handle
+def zipdir(paths, zip_file):
+    """
+    Archives files to zip
+    """
     for path in paths:
         empty_paths = []
-        if not (os.path.exists(path)):
+        if not os.path.exists(path):
             empty_paths.append(path)
 
-        for root, dirs, files in os.walk(path):
+        for root, _, files in os.walk(path):
             for file in files:
-                ziph.write(os.path.join(root, file),
-                           os.path.relpath(os.path.join(root, file),
-                                           os.path.join(path, '..')))
-        if (len(empty_paths) != 0):
+                zip_file.write(os.path.join(root, file),
+                               os.path.relpath(os.path.join(root, file),
+                                               os.path.join(path, '..')))
+
+        if len(empty_paths) != 0:
             raise NameError("some folders did not exist!")
 
 
-# folder принимает arhive.py как аргумент.
-folders1 = sys.argv[1:]
-folders = []
-for path in folders1:
-    folders.append('img/' + path)
-# Рандомное название строки для того чтобы не было повторов.
-std = str(uuid.uuid4()) + ".zip"
+def get_folders(folders):
+    """
+    Gets paths from system args and renames them
+    """
+    if not folders:
+        folders = sys.argv[1:]
+    for key, path in enumerate(folders):
+        folders[key] = 'img/' + path
 
-with zipfile.ZipFile(std, 'w', zipfile.ZIP_DEFLATED) as zipf:
-    zipdir(folders, zipf)
+
+def main(folders=None):
+    """
+    Main function
+    """
+    if folders is None:
+        folders = []
+    get_folders(folders)
+    zip_name = str(uuid.uuid4()) + ".zip"
+
+    with zipfile.ZipFile(zip_name, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+        zipdir(folders, zip_file)
+
+
+if __name__ == '__main__':
+    main(["dog"])
