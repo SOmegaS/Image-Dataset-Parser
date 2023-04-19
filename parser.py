7ca0849b1002a62bf6e22f30149d775dc13e40f7
semenishchev-ai <113626930+semenishchev-ai@users.noreply.github.com>
2023-04-19 18:20:14 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,19 +1,21 @@
+import json
 import os
 import sys
 import httplib2
 import requests
-from parsers import unsplash
-from parsers import openverse
-from parsers import vecteezy
-from parsers import publicdomain
-from parsers import unsplash
-from parsers import wikimedia
-from parsers import google
+from parsers.burst import search_on_burst
+from parsers.google import search_on_google
+from parsers.openverse import search_on_openverse
+from parsers.publicdomain import search_on_publicdomain
+from parsers.unsplash import search_on_unsplash
+from parsers.vecteezy import search_on_vecteezy, search_images_by_links
+from parsers.wikimedia import search_on_wikimedia
 
-amount_const = 7
-sz = [1 / 7] * 7
-path = "./images"
-func = [search_on_unsplash, parse_page, search_on_wikimedia, search_on_burst, search_on_vecteezy, search_on_google, search_on_openverse]
+path = "../images/"
+func = [search_on_unsplash, search_on_openverse, search_on_wikimedia, search_on_publicdomain, search_on_burst]
+amount_const = len(func)
+part = 1 / amount_const
+sz = [part] * (amount_const - 1)
 
 
 def make_dirs(query, dirs_path):  # copypaste from Parser1
@@ -42,26 +44,54 @@ def download_without_cash(url, output_name):
     out.close()
 
 
-def parse(request, size, links):
+def parse(query):
     for i in range(amount_const):
-        for index, link in enumerate(func[i](request, size * sz[i])):  # get links array
+        links_to_download = dict()
+        links_to_images = dict()
+        done = 0
+        for query_name, query_number in query.items():
+            if i + 1 == amount_const:
+                # num = query_number - done
+                num = round(query_number * sz[i - 1])
+                done = query_number
+            else:
+                num = round(query_number * sz[i])
+                done += num
+            if func[i] == search_on_publicdomain:
+                search_on_publicdomain(query_name, query_number, path, i, num)
+                continue
             if func[i] == search_on_vecteezy:
-                download_without_cash(link, f"{path}{request}/{index}.jpg")
-            download(link, f"{path}{request}/{index}.jpg")  # download for each parser
+                links_to_images[query_name] = search_on_vecteezy(query_name, num)
+                links_to_download[query_name] = search_images_by_links(links_to_images[query_name],
+                                                                       num)
+            else:
+                links_to_download[query_name] = func[i](query_name, num)
+        for name, links in links_to_download.items():
+            for index, link in enumerate(links):  # get links array
+                if func[i] == search_on_vecteezy:
+                    download_without_cash(link, f"{path}{name}/{index}_parser{i}.jpg")
+                if func[i] == search_on_openverse:
+                    link = link['image_link']
+                download(link, f"{path}{name}/{index}_parser{i}.jpg")  # download for each parser
+            if func[i] == search_on_openverse:
+                with open(f"{path}{name}/authority.json", "a", encoding="utf-8") as file:
+                    json.dump(links_to_download, file, indent=4, ensure_ascii=False)
 
 
 def main(args=None):
-    if args is None:
-        args = sys.argv
-    query = {}
-    count = args[-1]
-    for cls in args[:-1]:
-        query[cls] = count
+    # if args is None:
+    #     args = sys.argv
+    # query = {}
+    # count = args[-1]
+    # for cls in args[:-1]:
+    #     query[cls] = count
+    query = {
+        "cat": 20,
+        "dog": 20
+    }
     dirs_path = path
     make_dirs(query, dirs_path)  # make directories first for all parsers output
-    links = {}
-    for (request, size) in query.items():
-        parse(request, size, links)
+    parse(query)
 
 
 if __name__ == "__main__":
